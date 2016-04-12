3944fba309cd2c2aefc2ba3a40d0afefc3edbe63
enricobacis <enrico.bacis@gmail.com>
2016-04-12 16:51:18 +0200

    refactor middleware

--- swift/common/crypto_utils.py
+++ swift/common/crypto_utils.py
@@ -1,31 +1,25 @@
 #!/usr/bin/env python
 
 import base64
-from Crypto.Cipher import AES
-from Crypto.Util import Counter
 from Crypto import Random
+from Crypto.Util import Counter
+from Crypto.Cipher import AES
 
-
+KEY_SIZE = 16
 BLOCK_SIZE = 16
 
 
-class CryptoService:
+def generate_random_key(key_size=KEY_SIZE):
+    """
+    Generate a random AES key (the BEL key)
+    """
+    return Random.get_random_bytes(key_size)
 
-    def generate_token(self):
-        """
-        Generate a random AES key (the BEL key)
-        """
-        random_bytes = Random.get_random_bytes(BLOCK_SIZE)
-        secret = base64.b64encode(random_bytes)
-        return secret
-
-    def encrypt_object(self, plaintext, key, file_name=False):
-        """
-        Encrypt a message using AES CTR
-        """
-        cipher = AES.new(key, AES.MODE_CTR, counter=Counter.new(BLOCK_SIZE * 8))
-        encoded = cipher.encrypt(plaintext)
-        # Encoding base32 to avoid paths (names containing slashes /)
-        if file_name:
-            encoded = base64.b32encode(encoded)
-        return encoded
+def encrypt_object(plaintext, key, encode=False, block_size=BLOCK_SIZE):
+    """
+    Encrypt a message using AES CTR
+    """
+    cipher = AES.new(key, AES.MODE_CTR, counter=Counter.new(block_size * 8))
+    ciphertext = cipher.encrypt(plaintext)
+    # Encoding base32 to avoid paths (names containing slashes /)
+    return base64.b32encode(ciphertext) if not encode else ciphertext
--- swift/common/middleware/sel_encryption.py
+++ swift/common/middleware/sel_encryption.py
@@ -1,11 +1,11 @@
 from swift.common.swob import wsgify
-from swift.common.crypto_utils import CryptoService
+from swift.common.crypto_utils import generate_random_key, encrypt_object
 from swift.proxy.controllers.base import get_container_info
 from swift.common.request_helpers import get_sys_meta_prefix
 
 META_OE = 'oe-version'
-SYSMETA_OBJ_OE = get_sys_meta_prefix('object') + META_OE
-SYSMETA_CONTAINER_OE = get_sys_meta_prefix('container') + META_OE
+SYS_OBJ = get_sys_meta_prefix('object') + META_OE
+SYS_CONT = get_sys_meta_prefix('container') + META_OE
 
 
 class SEL_Encryption():
@@ -13,43 +13,42 @@ class SEL_Encryption():
     def __init__(self, app, conf):
         self.app = app
         self.conf = conf
-        self.cs = CryptoService()
 
     @wsgify
     def __call__(self, req):
         version, account, container, obj = req.split_path(1, 4, True)
 
-        if container is not None:
+        if obj:
             container_info = get_container_info(req.environ, self.app)
 
         # PUT container
         if container and not obj and req.method == 'PUT':
-            req.headers[SYSMETA_CONTAINER_OE] = '0'
+            req.headers[SYS_CONT] = '0'
 
         # PUT object
         if obj and req.method == 'PUT':
-            req.headers[SYSMETA_OBJ_OE] = container_info['sysmeta'].get(META_OE, '0')
+            req.headers[SYS_OBJ] = container_info['sysmeta'].get(META_OE, '0')
 
         # pass request to next wsgi middleware and get response
         resp = req.get_response(self.app)
 
         # HEAD/GET container
         if container and not obj and req.method in ('HEAD', 'GET'):
-            resp.headers['X-' + META_OE] = resp.headers.get(SYSMETA_CONTAINER_OE, '')
+            resp.headers['X-' + META_OE] = resp.headers.get(SYS_CONT, '')
 
         # HEAD object
         if obj and req.method == 'HEAD':
-            resp.headers['X-' + META_OE] = resp.headers.get(SYSMETA_OBJ_OE, '')
+            resp.headers['X-' + META_OE] = resp.headers.get(SYS_OBJ, '')
 
         # GET object (OverEncryption)
         if req.method == 'GET' and obj is not None:
             cont_version = container_info['sysmeta'].get(META_OE, '')
-            obj_version = resp.headers.get(SYSMETA_OBJ_OE, '')
-            resp.headers['X-' + META_OE] = resp.headers.get(SYSMETA_OBJ_OE, '')
+            obj_version = resp.headers.get(SYS_OBJ, '')
+            resp.headers['X-' + META_OE] = resp.headers.get(SYS_OBJ, '')
             if cont_version != obj_version:
-                sel_key = self.cs.generate_token()
-                resp.body = self.cs.encrypt_object(resp.body, sel_key)
-                resp.headers['X-SEL-Key'] = sel_key
+                sel_key = generate_random_key()
+                resp.body = encrypt_object(resp.body, sel_key)
+                resp.headers['X-SEL-Key'] = sel_key.encode('base64')
 
         return resp
 
