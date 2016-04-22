5588e6c1f54ae0ea586ddc4fbee9ac1da1643ce6
enricobacis <enrico.bacis@gmail.com>
2016-04-22 23:49:42 +0200

    add the whole logic

--- swift/common/middleware/sel_encryption.py
+++ swift/common/middleware/sel_encryption.py
@@ -1,4 +1,5 @@
-from cStringIO import StringIO
+from io import BytesIO
+from itertools import tee
 from swift.common.swob import wsgify
 from swift.common.http import is_success
 from swift.common.overencryption_utils import generate_random_key, encrypt_object, revoking_users
@@ -9,12 +10,16 @@ META_OE = 'oe-version'
 SYS_OBJ = get_sys_meta_prefix('object') + META_OE
 SYS_CONT = get_sys_meta_prefix('container') + META_OE
 
+META_KEY = 'SEL-Key'
+SYS_KEY = get_sys_meta_prefix('object') + META_KEY
+
 
 class SEL_Encryption():
 
     def __init__(self, app, conf):
         self.app = app
         self.conf = conf
+        self.materialize = self.conf['materialize']
 
     @wsgify
     def __call__(self, req):
@@ -53,32 +58,59 @@ class SEL_Encryption():
         if req.method == 'GET' and obj:
             cont_version = container_info['sysmeta'].get(META_OE, '')
             obj_version = resp.headers.get(SYS_OBJ, '')
-            resp.headers['X-' + META_OE] = resp.headers.get(SYS_OBJ, '')
-            if cont_version != obj_version:
-                sel_key = generate_random_key()
-                resp.app_iter = encrypt_object(resp.app_iter, sel_key)
-                resp.headers['X-SEL-Key'] = sel_key.encode('base64')
 
-                # do the materialization
-                if 'X-SEL-Materialize' in req.headers:
-                    data = 'OVERWRITTEN' # debug
-                    destination = resp.environ['PATH_INFO']
+            if cont_version == obj_version: # SEL not needed, add oe header
+                resp.headers['X-' + META_OE] = obj_version
 
+            else: # SEL needed
+                new_sel_key = generate_random_key()
+
+                if not self.materialize:  # DOE
+                    resp.app_iter = encrypt_object(resp.app_iter, new_sel_key)
+                    resp.headers['X-' + META_KEY] = sel_key.encode('base64')
+
+                else: # SOE (do the materialization)
+
+                    # decrypt the previously encrypted object (if needed)
+                    old_sel_key = resp.headers.get(SYS_KEY, '')
+                    data = (decrypt_object(resp.app_iter, old_sel_key)
+                            if old_sel_key else resp.app_iter)
+
+                    # encrypt the data with the new key iteratevly
+                    app_iter = encrypt_object(data, new_sel_key)
+                    app_iter1, app_iter2 = tee(app_iter)
+
+                    # create the input for the PUT request
+                    wsgi_input, length = BytesIO(), 0
+                    for chunk in app_iter1:
+                        length += wsgi_input.write(chunk)
+                    wsgi_input.seek(0) # seek to the beginning
+
+                    # create the PUT environment
+                    path = resp.environ['PATH_INFO']
                     new_env = req.environ.copy()
                     new_env['REQUEST_METHOD'] = 'PUT'
-                    new_env['PATH_INFO'] = destination
-                    new_env['wsgi.input'] = StringIO(data) # it has readline
-                    new_env['CONTENT_LENGTH'] = len(data)
+                    new_env['PATH_INFO'] = path
+                    new_env['wsgi.input'] = wsgi_input
+                    new_env['CONTENT_LENGTH'] = length
                     new_env['swift.source'] = 'SEL'
                     new_env['HTTP_USER_AGENT'] = \
                         '%s SelEncryption' % req.environ.get('HTTP_USER_AGENT')
 
-                    put_obj_req = Request.blank(destination, new_env)
+                    # send the PUT request and obtain its response. We also
+                    # store the SEL key and update the oe-version
+                    headers = { SYS_OBJ: cont_version, SYS_KEY: new_sel_key }
+                    put_obj_req = Request.blank(path, new_env, headers=headers)
                     put_obj_resp = put_obj_req.get_response(self.app)
 
                     # for debug purposes, return the PUT response if not success
                     if not is_success(put_obj_resp.status_int):
                         return put_obj_resp
+
+                    # update the response using the new encrypted app_iter
+                    resp.app_iter = app_iter2
+                    resp.headers['X-' + META_OE] = cont_version
+                    resp.headers['X-' + META_KEY] = new_sel_key
 
         return resp
 
